c8069f6e258f5df5717de7f6734975ff122d424f
chenhaizhuang <chinesechain@163.com>
2024-02-20 15:28:42 +0800

    结果类创建

---
+++ src/main/java/com/chaincat/pay/model/base/PayResult.java
@@ -0,0 +1,29 @@
+package com.chaincat.pay.model.base;
+
+import lombok.Data;
+
+/**
+ * 支付结果
+ *
+ * @author chenhaizhuang
+ */
+@Data
+public class PayResult<T> {
+
+    /**
+     * ID，对应orderId和refundId
+     */
+    private String id;
+
+    /**
+     * 数据，不同支付渠道的查询对象
+     */
+    private T data;
+
+    public static <T> PayResult<T> of(String id, T data) {
+        PayResult<T> payResult = new PayResult<>();
+        payResult.setId(id);
+        payResult.setData(data);
+        return payResult;
+    }
+}
