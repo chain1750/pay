443ec63119cdf084f93f9fedcd69789892382316
chenhaizhuang <chinesechain@163.com>
2024-02-20 15:18:53 +0800

    枚举创建

---
+++ src/main/java/com/chaincat/pay/model/enums/OrderStateEnum.java
@@ -0,0 +1,24 @@
+package com.chaincat.pay.model.enums;
+
+/**
+ * 订单状态枚举
+ *
+ * @author chenhaizhuang
+ */
+public enum OrderStateEnum {
+
+    /**
+     * 未支付
+     */
+    NOT_PAY,
+
+    /**
+     * 已支付
+     */
+    SUCCESS,
+
+    /**
+     * 已关闭
+     */
+    CLOSED
+}
---
+++ src/main/java/com/chaincat/pay/model/enums/RefundStateEnum.java
@@ -0,0 +1,24 @@
+package com.chaincat.pay.model.enums;
+
+/**
+ * 退款状态枚举
+ *
+ * @author chenhaizhuang
+ */
+public enum RefundStateEnum {
+
+    /**
+     * 处理中
+     */
+    PROCESSING,
+
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL
+}
